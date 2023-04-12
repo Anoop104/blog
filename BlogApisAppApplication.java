3ca1514c227586fdce3c33a365436dd08b9a7ddd
Anoop104 <47365320+Anoop104@users.noreply.github.com>
2023-04-12 13:50:45 +0530

    Add files via upload

---
+++ BlogApisAppApplication.java
@@ -0,0 +1,14 @@
+package com.codewithbackend.blog.blog.apis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApisAppApplication.class, args);
+		
+	}
+
+}
